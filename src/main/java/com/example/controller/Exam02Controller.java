99a2e0472205695599bfcaadfe49594df8cdb384
erikamikami <g.erika3up@gmail.com>
2022-05-12 09:52:54 +0900

    create exam02

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,13 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/exam02")
 public class Exam02Controller {
 	
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam02";
 	}
 	
 	@RequestMapping("/result")
-	public String result() {
-		return "result";
+	public String result(int firstFigure, int secondFigure) {
+		session.setAttribute("firstFigure", firstFigure);
+		session.setAttribute("secondFigure", secondFigure);
+		int result = firstFigure + secondFigure;
+		session.setAttribute("result", result);
+		return "exam02-result";
+	}
+	
+	@RequestMapping("/result2")
+	public String result2() {
+		return "exam02-result2";
 	}
 }
