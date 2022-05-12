42240e98d15c691572edea0dfaeef7902c25ba96
erikamikami <g.erika3up@gmail.com>
2022-05-12 10:28:10 +0900

    complete exam03

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import java.text.NumberFormat;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +19,23 @@ public class Exam03Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam03";
+	}
+	
+	@RequestMapping("/result")
+	public String result(int goods1, int goods2, int goods3) {
+		NumberFormat nfNum = NumberFormat.getNumberInstance();    //カンマ区切り形式
+		
+		// 税抜き価格
+		int noTaxResult = goods1 + goods2 + goods3;
+		String formatNoTaxResult = nfNum.format(noTaxResult);
+		
+		// 税込み価格(税率：10%）
+		int taxResult = (int) (noTaxResult*1.1);
+		String formatTaxResult = nfNum.format(taxResult);
+		
+		application.setAttribute("formatNoTaxResult", formatNoTaxResult);
+		application.setAttribute("formatTaxResult", formatTaxResult);
+		
+		return "exam03-result";
 	}
 }
