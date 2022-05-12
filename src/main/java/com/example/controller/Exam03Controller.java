b26453f0ed6260e53309bfc882f6e35f20bdbe2b
erikamikami <g.erika3up@gmail.com>
2022-05-12 10:01:00 +0900

    create exam03Controller, exam03.html

---
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam03")
+public class Exam03Controller {
+	
+	@Autowired
+	private ServletContext application;
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam03";
+	}
+}
